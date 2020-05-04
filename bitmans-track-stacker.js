11ec90606b18531a87f383097c61d41fb4a0d760
bitman2049 <aldrasio@gmail.com>
2020-05-03 22:35:41 -0500

    Add method to find ride construction window

--- bitmans-track-stacker.js
+++ bitmans-track-stacker.js
@@ -2,25 +2,35 @@
 
 // Bitman's Track Stacker, created 2020-05-01
 
-var MAX_COPIES = 50;
+var MAX_COPIES = 255;
 var MIN_COPIES = 1;
 var RIDE_CONSTRUCTION_ID = 13;
 var copyCount = MIN_COPIES;
 var offset = {x: 0, y: 0, z: 0};
 var Widgets;
 var mainWindow;
+var rcWindow;
+var rideName;
 
-function GetRideID() {
-	var wRideConstruct = getWindow()
+function GetRideConstructionWindow() {
+	for (var i = 0; i < ui.windows; i++)
+	{
+		var w = ui.getWindow(i);
+		if (w.classification === 13)
+		{
+			return w;
+		}
+	}
+	return null;
 }
 
-function GetWidget(widget) {
-    if (mainWindow) {
-        return mainWindow.findWidget(widget.name);
+function GetWidget(w, widget) {
+    if (w) {
+        return w.findWidget(widget.name);
     }
 }
 
-function updateSpinner(widget, value) {
+function UpdateSpinner(widget, value) {
 	if (!widget) {return; }
 	widget.text = value.toString();
 	console.log(value.toString());
@@ -50,8 +60,8 @@ function createWidgets() {
 		height: widgetHeight,
 		name: 'spinXOff',
 		text: offset.x.toString(),
-		onDecrement: function() {offset.x--; updateSpinner(GetWidget(xOffsetSpinner), offset.x)},
-		onIncrement: function() {offset.x++; updateSpinner(GetWidget(xOffsetSpinner), offset.x)},
+		onDecrement: function() {offset.x--; updateSpinner(GetWidget(mainWindow, xOffsetSpinner), offset.x)},
+		onIncrement: function() {offset.x++; updateSpinner(GetWidget(mainWindow, xOffsetSpinner), offset.x)},
 	};
 	
 	var yOffsetLabel = {
@@ -72,8 +82,8 @@ function createWidgets() {
 		height: widgetHeight,
 		name: 'spinYOff',
 		text: offset.y.toString(),
-		onDecrement: function() {offset.y--; updateSpinner(GetWidget(yOffsetSpinner), offset.y)},
-		onIncrement: function() {offset.y++; updateSpinner(GetWidget(yOffsetSpinner), offset.y)},
+		onDecrement: function() {offset.y--; updateSpinner(GetWidget(mainWindow, yOffsetSpinner), offset.y)},
+		onIncrement: function() {offset.y++; updateSpinner(GetWidget(mainWindow, yOffsetSpinner), offset.y)},
 	};
 	
 	var zOffsetLabel = {
@@ -94,8 +104,8 @@ function createWidgets() {
 		height: widgetHeight,
 		name: 'spinZOff',
 		text: offset.z.toString(),
-		onDecrement: function() {offset.z--; updateSpinner(GetWidget(zOffsetSpinner), offset.z)},
-		onIncrement: function() {offset.z++; updateSpinner(GetWidget(zOffsetSpinner), offset.z)},
+		onDecrement: function() {offset.z--; updateSpinner(GetWidget(mainWindow, zOffsetSpinner), offset.z)},
+		onIncrement: function() {offset.z++; updateSpinner(GetWidget(mainWindow, zOffsetSpinner), offset.z)},
 	};
 	
 	var copyCountLabel = {
@@ -116,8 +126,8 @@ function createWidgets() {
 		height: widgetHeight,
 		name: 'spinCopyCount',
 		text: copyCount.toString(),
-		onDecrement: function() {copyCount = Math.max(copyCount-1, MIN_COPIES); updateSpinner(GetWidget(copyCountSpinner), copyCount)},
-		onIncrement: function() {copyCount = Math.min(copyCount+1, MAX_COPIES); updateSpinner(GetWidget(copyCountSpinner), copyCount)},
+		onDecrement: function() {copyCount = Math.max(copyCount-1, MIN_COPIES); updateSpinner(GetWidget(mainWindow, copyCountSpinner), copyCount)},
+		onIncrement: function() {copyCount = Math.min(copyCount+1, MAX_COPIES); updateSpinner(GetWidget(mainWindow, copyCountSpinner), copyCount)},
 	};
 
 	var enableCheckbox = {
@@ -125,11 +135,32 @@ function createWidgets() {
 		x: 2,
 		y: 16 + (widgetHeight + 2) * 4,
 		width: 198,
-		height: 10,
+		height: widgetHeight,
 		name: 'chkEnable',
 		text: "Enabled",
-		isChecked: true
+		isChecked: true,
+		onChange: function(isChecked) {if (isChecked) rcWindow = GetRideConstructionWindow()}
 	};
+
+	var attachButton = {
+		type: 'button',
+		x: 2,
+		y: 16 + (widgetHeight + 2) * 5,
+		width: 198,
+		height: widgetHeight,
+		name: 'attachButton',
+		text: "Attach to Ride",
+		onClick: function() {rcWindow = GetRideConstructionWindow()}
+	}
+
+	var rideLabel = {
+		type: 'label',
+		x: 2,
+		y: 16 + (widgetHeight + 2) * 6,
+		width: 198,
+		height: widgetHeight,
+		text: rideName
+	}
 	
 	Widgets.push(xOffsetSpinner);
 	Widgets.push(xOffsetLabel);
@@ -140,6 +171,8 @@ function createWidgets() {
 	Widgets.push(copyCountSpinner);
 	Widgets.push(copyCountLabel);
 	Widgets.push(enableCheckbox);
+	Widgets.push(attachButton);
+	Widgets.push(rideLabel);
 };
 
 var openWindow = function() {
@@ -147,7 +180,7 @@ var openWindow = function() {
 	mainWindow = ui.openWindow({
 		classification: "custom.my",
 		width: 200,
-		height: 100,
+		height: 200,
 		title: "Bitman's Track Stacker",
 		widgets: Widgets
 	});
@@ -163,7 +196,7 @@ var main = function() {
 };
 
 registerPlugin({
-    name: 'Bitman\'s Track Stacker',
+    name: "Bitman's Track Stacker",
     version: '1.0',
     authors: ['bitman2049'],
     type: 'remote',
